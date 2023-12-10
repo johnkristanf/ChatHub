2fca3a795fb5463ae36e8d79b65f51d055a2175c
John Kristan <jkpogz09@gmail.com>
2023-12-10 16:50:36 +0800

    mag gym nako kapoy na

--- src/middleware/template.ts
+++ src/middleware/template.ts
@@ -2,7 +2,7 @@ import path from 'path';
 
 export default (app:any) => {
     app.set('view engine', 'ejs');
-    app.set('views', path.join(__dirname, 'views'));
+    app.set('views', path.join(__dirname, '../views'));
 
 
 }
